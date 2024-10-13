8b2452c3217263de6945cf466d5c2332dae44da2
Björn Wildeman <bwildeman99@gmail.com>
2024-10-14 01:29:10 +0200

    Initial Commit

---
+++ src/test/java/s_a_rb01_its6/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package s_a_rb01_its6.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
